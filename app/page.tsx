d8a2db5398e50cc4c4aff1849308db47496d38b6
Turjo-B <mhbt24803@gmail.com>
2026-02-18 14:22:53 +0600

    fixed issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import ProfileSetupModal from '@/app/components/ProfileSetupModal'
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
-
+const a = 0;
 export default async function Home() {
   const supabase = await createClient()
 
